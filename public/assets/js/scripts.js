ba350d7464ad0692341f82d089cb0b9cd3f21723
abzikel <pikako0619@gmail.com>
2025-09-21 11:54:33 -0600

    Translation added to all pages

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -9,9 +9,39 @@ document.addEventListener('DOMContentLoaded', function () {
                     placeholder.innerHTML = data;
                 }
             })
-            .catch(error => console.error('Error al cargar el componente:', error));
+            .catch(error => console.error('Error loading the component:', error));
+    }
+
+    // Function to set language on 404 page
+    function setInitialLanguageFor404() {
+        // Try to get the elements
+        const langEs404 = document.getElementById('lang-es-404');
+        const langEn404 = document.getElementById('lang-en-404');
+
+        // If we are not on 404 page, return
+        if (!langEs404 || !langEn404) return;
+
+        // Get user language
+        const htmlTag = document.documentElement;
+        const titleTag = document.querySelector('title');
+        const userLang = navigator.language.substring(0, 2);
+
+        if (userLang === 'es') {
+            // Show content on spanish
+            htmlTag.lang = 'es';
+            titleTag.textContent = 'Página no Encontrada - Sistema ETR';
+            langEs404.style.display = 'block';
+        } else {
+            // Show content on english
+            htmlTag.lang = 'en';
+            titleTag.textContent = 'Page Not Found - Sistema ETR';
+            langEn404.style.display = 'block';
+        }
     }
 
     // Load footer
     loadComponent('footer.html', 'footer-placeholder');
+
+    // Set language for 404 error
+    setInitialLanguageFor404();
 });
